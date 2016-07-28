64472ac7ec9f89414ccf83a51ff60de9afb16fe5
Fanani M. Ihsan <fanani.mi@gmail.com>
2016-07-28 17:41:23 +0700

    setup templates

--- website/views.py
+++ website/views.py
@@ -5,3 +5,33 @@ from website import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/jobs/')
+def jobs():
+    return render_template('jobs.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logout/')
+def logout():
+    pass
+
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/forgot-password/')
+def forgot_password():
+    return render_template('forgot_password.html')
